290879fe7c8f1273ac5ef29b40a8c648921971aa
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-07-04 00:07:51 +0200

    login form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ function Navbar () {
     return ( 
         <nav className={styles.navbar}>
             <ul>
-                <li className={styles.title}>myMoney</li>
+                <li className={styles.title}><Link to="/">myMoney</Link></li>
 
-                <li><Link to="/login"></Link>Login</li>
+                <li><Link to="/login">Login</Link></li>
                 <li><Link to="/signup">Signup</Link></li>
             </ul>
         </nav>

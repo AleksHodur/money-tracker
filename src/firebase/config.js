0f01a6203d93c0341d1541cc7cff4f37733ba008
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-07-04 10:44:32 +0200

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCoDDJZiKpgobRxdK6K6RhGfK3P5M1RY0w",
+    authDomain: "mymoney-8d010.firebaseapp.com",
+    projectId: "mymoney-8d010",
+    storageBucket: "mymoney-8d010.appspot.com",
+    messagingSenderId: "388346229047",
+    appId: "1:388346229047:web:c1718d2ef5f2b3b5f66ab2"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };

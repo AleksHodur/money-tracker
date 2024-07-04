59d785dc570756e3425bedd580dbb26781a0fa17
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-07-04 10:48:54 +0200

    firebase auth config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCoDDJZiKpgobRxdK6K6RhGfK3P5M1RY0w",
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig);
 
 // init service
 const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFirestore };
+export { projectFirestore, projectAuth };

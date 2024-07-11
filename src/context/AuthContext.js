a41c83564efbf17b0ebf3a390004da7b33b2515e
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-07-11 21:17:03 +0200

    checking user in the first load

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
+import { projectAuth } from '../firebase/config';
 
 export const AuthContext = createContext();
 
@@ -11,6 +12,10 @@ export const authReducer = (state, action) => {
         case 'LOGOUT':
             return { ...state, user: null };
 
+        case 'AUTH_IS_READY':
+            return { ...state, user: action.payload, authIsReady: true }
+
+
         default:
             return state;
     }
@@ -19,9 +24,17 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(authReducer, {
-        user: null
+        user: null,
+        authIsReady: false
     });
     console.log('Auth Context state: ', state);
+
+    useEffect(() => {
+        const unsub = projectAuth.onAuthStateChanged((user) => {
+            dispatch({ type: 'AUTH_IS_READY', payload: user });
+            unsub();
+        });
+    }, []);
     
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>

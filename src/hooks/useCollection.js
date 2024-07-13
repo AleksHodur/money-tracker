9a763d23a215f4400bbeb91e14e95fdd6d4d72b1
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-07-14 00:29:16 +0200

    unique user info

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,14 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = (collection) => {
+export const useCollection = (collection, _query) => {
 
     const [documents, setDocuments] = useState(null);
     const [error, setError] = useState(null);
 
+    /* This is done so the the ueEffect doesn't trigger each time query
+    is mounted on its own component, avoiding infinite loops */
+    const query = useRef(_query).current;
+
     // fires whenever the collection changes
     useEffect(() => {
         let ref = projectFirestore.collection(collection);
+
+        if(query){
+            ref = ref.where(...query);
+        }
 
         // real-time subscription
         const unsubscribe = ref.onSnapshot((snapshot) => {
@@ -27,7 +35,7 @@ export const useCollection = (collection) => {
         // unsubscribe on unmount: so when we exit the page, this cycle stops
         return () => unsubscribe();
 
-    }, [collection]);
+    }, [collection, query]); //all outside variables must be dependencies
 
     return { documents, error };
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,7 +7,10 @@ import { useCollection } from '../../hooks/useCollection';
 function Home () {
 
     const { user } = useAuthContext();
-    const { documents, error } = useCollection('transactions');
+    const { documents, error } = useCollection(
+        'transactions',
+        ["uid", "==", user.uid]
+    );
     console.log('documents and error', documents, error);
 
     return ( 
